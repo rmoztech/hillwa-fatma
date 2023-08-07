8621b85b4c077099e66774084667f9465be94d34
FatmaZaher <fatmaabdulhameid@gmail.com>
2023-08-07 14:08:35 +0300

    responsive design and edit modal

--- js/main.js
+++ js/main.js
@@ -111,9 +111,146 @@ $(document).ready(function () {
 
 
 
+    $(".complete-sale-btn").click(function (e) {
+        e.preventDefault();
+        var count = $(".shopping-cart-page").find(".order-card").length;
+        if (count < 10) {
+            var toastElList = [].slice.call($('.toast'))
+            var toastList = toastElList.map(function (toastEl) {
+                return new bootstrap.Toast(toastEl)
+            })
+            toastList.forEach(toast => toast.show())
+        }else{
+            $("#logInModal").modal("show");
+        }
+    });
+
+
+    $(".logInForm").validate({
+        rules: {
+            mobile: {
+                required: true,
+            },
+        },
+
+        messages: {
+            mobile: {
+                required: "يرجى ادخال رقم الموبايل",
+            },
+        }
+    });
+
+    $('.logInForm').on('submit', function (e) {
+        e.preventDefault();
+        if ($(".logInForm").valid()) {
+            $("#logInModal").modal("hide");
+        }
+    });
+
+    $(".newAccountForm").validate({
+        rules: {
+            mobile: {
+                required: true,
+            },
+            username: {
+                required: true
+            }
+        },
+
+        messages: {
+            mobile: {
+                required: "يرجى ادخال رقم الموبايل",
+            },
+            username: {
+                required: "يرجى ادخال الاسم"
+            }
+        }
+    });
+
+    $('.newAccountForm').on('submit', function (e) {
+        e.preventDefault();
+        if ($(".newAccountForm").valid()) {
+            $("#newAccountModal").modal("hide");
+            $("#codeModal").modal("show");
+        }
+    });
+
     AOS.init({
         duration: 1500,
         once: true,
     });
+
 });
 
+
+const numberCodeForm = document.querySelector('[data-number-code-form]');
+const numberCodeInputs = [...numberCodeForm.querySelectorAll('[data-number-code-input]')];
+
+// Event callbacks
+const handleInput = ({target}) => {
+  if (!target.value.length) { return target.value = null; }
+  
+  const inputLength = target.value.length;
+  let currentIndex = Number(target.dataset.numberCodeInput);
+  
+  if (inputLength > 1) {
+    const inputValues = target.value.split('');
+    
+    inputValues.forEach((value, valueIndex) => {
+      const nextValueIndex = currentIndex + valueIndex;
+      
+      if (nextValueIndex >= numberCodeInputs.length) { return; }
+      
+      numberCodeInputs[nextValueIndex].value = value;
+    });
+    
+    currentIndex += inputValues.length - 2;
+  }
+ 
+  const nextIndex = currentIndex + 1;
+  
+  if(nextIndex < numberCodeInputs.length) {
+    numberCodeInputs[nextIndex].focus();
+  }
+}
+
+const handleKeyDown = e => {
+  const {code, target} = e;
+  
+  const currentIndex = Number(target.dataset.numberCodeInput);
+  const previousIndex = currentIndex - 1;
+  const nextIndex = currentIndex + 1;
+  
+  const hasPreviousIndex = previousIndex >= 0;
+  const hasNextIndex = nextIndex <= numberCodeInputs.length - 1
+  
+  switch(code) {
+    case 'ArrowLeft':
+    case 'ArrowUp':
+      if (hasPreviousIndex) {
+        numberCodeInputs[previousIndex].focus();
+      }
+      e.preventDefault();
+      break;
+      
+    case 'ArrowRight':
+    case 'ArrowDown':
+      if (hasNextIndex) {
+        numberCodeInputs[nextIndex].focus();
+      }
+      e.preventDefault();
+      break;
+    case 'Backspace':
+      if (!e.target.value.length && hasPreviousIndex) {
+        numberCodeInputs[previousIndex].value = null;
+        numberCodeInputs[previousIndex].focus();
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+// Event listeners
+numberCodeForm.addEventListener('input', handleInput);
+numberCodeForm.addEventListener('keydown', handleKeyDown);

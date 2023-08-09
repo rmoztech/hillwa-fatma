3472764983dfd2e9269340767e6d128492078533
FatmaZaher <fatmaabdulhameid@gmail.com>
2023-08-09 14:13:52 +0300

    profile page

--- js/main.js
+++ js/main.js
@@ -109,8 +109,6 @@ $(document).ready(function () {
         }
     });
 
-
-
     $(".complete-sale-btn").click(function (e) {
         e.preventDefault();
         var count = $(".shopping-cart-page").find(".order-card").length;
@@ -120,11 +118,10 @@ $(document).ready(function () {
                 return new bootstrap.Toast(toastEl)
             })
             toastList.forEach(toast => toast.show())
-        }else{
+        } else {
             $("#logInModal").modal("show");
         }
     });
-
 
     $(".logInForm").validate({
         rules: {
@@ -175,6 +172,11 @@ $(document).ready(function () {
         }
     });
 
+    $(function () {
+        $("#datepicker").datepicker();
+    });
+
+
     AOS.init({
         duration: 1500,
         once: true,
@@ -187,68 +189,68 @@ const numberCodeForm = document.querySelector('[data-number-code-form]');
 const numberCodeInputs = [...numberCodeForm.querySelectorAll('[data-number-code-input]')];
 
 // Event callbacks
-const handleInput = ({target}) => {
-  if (!target.value.length) { return target.value = null; }
-  
-  const inputLength = target.value.length;
-  let currentIndex = Number(target.dataset.numberCodeInput);
-  
-  if (inputLength > 1) {
-    const inputValues = target.value.split('');
-    
-    inputValues.forEach((value, valueIndex) => {
-      const nextValueIndex = currentIndex + valueIndex;
-      
-      if (nextValueIndex >= numberCodeInputs.length) { return; }
-      
-      numberCodeInputs[nextValueIndex].value = value;
-    });
-    
-    currentIndex += inputValues.length - 2;
-  }
- 
-  const nextIndex = currentIndex + 1;
-  
-  if(nextIndex < numberCodeInputs.length) {
-    numberCodeInputs[nextIndex].focus();
-  }
+const handleInput = ({ target }) => {
+    if (!target.value.length) { return target.value = null; }
+
+    const inputLength = target.value.length;
+    let currentIndex = Number(target.dataset.numberCodeInput);
+
+    if (inputLength > 1) {
+        const inputValues = target.value.split('');
+
+        inputValues.forEach((value, valueIndex) => {
+            const nextValueIndex = currentIndex + valueIndex;
+
+            if (nextValueIndex >= numberCodeInputs.length) { return; }
+
+            numberCodeInputs[nextValueIndex].value = value;
+        });
+
+        currentIndex += inputValues.length - 2;
+    }
+
+    const nextIndex = currentIndex + 1;
+
+    if (nextIndex < numberCodeInputs.length) {
+        numberCodeInputs[nextIndex].focus();
+    }
 }
 
 const handleKeyDown = e => {
-  const {code, target} = e;
-  
-  const currentIndex = Number(target.dataset.numberCodeInput);
-  const previousIndex = currentIndex - 1;
-  const nextIndex = currentIndex + 1;
-  
-  const hasPreviousIndex = previousIndex >= 0;
-  const hasNextIndex = nextIndex <= numberCodeInputs.length - 1
-  
-  switch(code) {
-    case 'ArrowLeft':
-    case 'ArrowUp':
-      if (hasPreviousIndex) {
-        numberCodeInputs[previousIndex].focus();
-      }
-      e.preventDefault();
-      break;
-      
-    case 'ArrowRight':
-    case 'ArrowDown':
-      if (hasNextIndex) {
-        numberCodeInputs[nextIndex].focus();
-      }
-      e.preventDefault();
-      break;
-    case 'Backspace':
-      if (!e.target.value.length && hasPreviousIndex) {
-        numberCodeInputs[previousIndex].value = null;
-        numberCodeInputs[previousIndex].focus();
-      }
-      break;
-    default:
-      break;
-  }
+    const { code, target } = e;
+
+    const currentIndex = Number(target.dataset.numberCodeInput);
+    const previousIndex = currentIndex - 1;
+    const nextIndex = currentIndex + 1;
+
+    const hasPreviousIndex = previousIndex >= 0;
+    const hasNextIndex = nextIndex <= numberCodeInputs.length - 1
+
+    switch (code) {
+        case 'ArrowLeft':
+        case 'ArrowUp':
+            if (hasPreviousIndex) {
+                numberCodeInputs[previousIndex].focus();
+            }
+            e.preventDefault();
+            break;
+
+        case 'ArrowRight':
+        case 'ArrowDown':
+            if (hasNextIndex) {
+                numberCodeInputs[nextIndex].focus();
+            }
+            e.preventDefault();
+            break;
+        case 'Backspace':
+            if (!e.target.value.length && hasPreviousIndex) {
+                numberCodeInputs[previousIndex].value = null;
+                numberCodeInputs[previousIndex].focus();
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 // Event listeners

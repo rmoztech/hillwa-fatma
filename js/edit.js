69ba1b43e8a5e6af05404e79185c73a691af4ec1
FatmaZaher <fatmaabdulhameid@gmail.com>
2023-08-28 15:26:49 +0300

    edit cards

--- js/edit.js
+++ js/edit.js
@@ -110,6 +110,39 @@ $(document).ready(function () {
         }
     })
 
+
+    $(function() {
+        if($('.addresses .owl-addresses .item').length <= 3){
+            $('.addresses .owl-theme').hide()
+        }else{
+            $('.addresses .owl-theme').show()
+        }
+    })
+
+    $('.owl-addresses').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        rtl: true,
+        items: 3,
+        dots: false,
+        navText: [
+            '<i class="fa-solid fa-chevron-left"></i>',
+            '<i class="fa-solid fa-chevron-right"></i>'
+        ],
+        navContainer: '.news-custom-nav',
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
 })
 
 
